995df40fefa3eb48f8faa11d9d3d786ca72b9424
Gabriel Affonso <36745191+gabzaf@users.noreply.github.com>
2022-05-31 17:53:45 +0100

    ExploringExceptions

---
+++ ExplorandoExceptions/Divisor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExplorandoExceptions
+{
+    public class Divisor
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int Divide()
+        {
+            int c = a / b;
+            return c;
+        }
+    }
+}
---
+++ ExplorandoExceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExplorandoExceptions
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Divisor divisor = new Divisor();
+            divisor.a = 10;
+            divisor.b = 0;
+            try
+            {
+                double divisor2 = divisor.Divide();
+            }
+            catch (DivideByZeroException) {
+
+                Console.WriteLine("Tentou dividir por zero! ");
+            }
+        }
+    }
+}
