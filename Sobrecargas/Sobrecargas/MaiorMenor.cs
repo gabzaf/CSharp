c6837f95b60495b1006f7e720d8021051931d2c1
Gabriel Affonso <36745191+gabzaf@users.noreply.github.com>
2022-05-31 14:44:57 +0100

    Testing overload

---
+++ Sobrecargas/Sobrecargas/MaiorMenor.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sobrecargas
+{
+    public class MaiorMenor
+    {
+        public double Maior(double a, double b)
+        {
+            if(a > b) return a;
+            return b;
+        }
+        public double Maior(double a, double b, double c)
+        {
+            if (a > b)
+            {
+                if (a > c)
+                    return a;
+                return c;
+            }
+
+            if (c > b)
+                return c;
+            return b;
+        }
+        public double Menor(double a, double b)
+        {
+            if (a < b) return a;
+            return b;
+        }
+        public double Menor(double a, double b, double c)
+        {
+            if (a < b)
+            {
+                if (a < c)
+                {
+                    return a;
+                }
+                return c;
+            }
+
+            if (c < b)
+            {
+                return c;
+            }
+            return b;
+        }
+    }
+}
---
+++ Sobrecargas/Sobrecargas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sobrecargas
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            double primeiroMaior = 0;
+            double segundoMaior = 0;
+            double menor = 0;
+
+            MaiorMenor maiorMenor = new MaiorMenor();
+
+            primeiroMaior = maiorMenor.Maior(a, b);
+            segundoMaior = maiorMenor.Maior(a, b, c);
+
+
+            Console.WriteLine(primeiroMaior);
+            Console.WriteLine(segundoMaior);
+
+        }
+    }
+}
+
