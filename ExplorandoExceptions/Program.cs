a1674ea65f43acd4e3159dccca52da9393111939
Gabriel Affonso <36745191+gabzaf@users.noreply.github.com>
2022-06-03 16:22:48 +0100

    Exceptions understood for now

--- ExplorandoExceptions/Program.cs
+++ ExplorandoExceptions/Program.cs
@@ -6,35 +6,38 @@ namespace ExplorandoExceptions
     {
         static void Main(string[] args)
         {
-            Divisor divisor = new Divisor(10, 0);
+            Divisor divisor = new Divisor(10, 2);
             Divisor divisor2 = null;
+
             try
             {
-                int resultado = divisor.Divide();
-                Console.WriteLine("O resultado da divisão é: " + resultado);
+                int resultado = Teste(divisor);
+                Console.WriteLine("O resultado é: " + resultado);
+                int numero = divisor2.Divide();
             }
             catch (DivideByZeroException e)
             {
                 Console.WriteLine("Não é possível dividir por zero!");
+                Console.WriteLine(e.StackTrace); ;
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Exceção de referência nula");
                 Console.WriteLine(e.StackTrace);
             }
-            Teste(divisor2);
             Console.ReadLine();
         }
         private static int Teste(Divisor a)
-            {
+        {
             try
             {
                 return a.Divide();
             }
-            catch (NullReferenceException e)
+            catch (DivideByZeroException)
             {
-                Console.WriteLine("Não pode passar referência nula!");
-                Console.WriteLine(e.StackTrace);
+                Console.WriteLine("Exceção com divisor = 0");
                 throw;
-             
             }
-                
         }
     }
 }
